74f75bbcf8e8f9d76ba32f2c85f512596b4aaf1b
GeethaMatheshwaran <msangeethaece2001@gmail.com>
2023-09-07 10:24:59 +0530

    show the testimonial slider only home page - related files are commited

--- APSGARLANDS/Modules/Testimonial/Http/Controllers/TestimonialController.php
+++ APSGARLANDS/Modules/Testimonial/Http/Controllers/TestimonialController.php
@@ -32,7 +32,7 @@ class TestimonialController extends Controller
 
     public function store(Request $request)
     {
-dd($request->name);
+
         Testimonial::create([
             'user_id' => auth()->id(),
             'user_name' => $request->name,
