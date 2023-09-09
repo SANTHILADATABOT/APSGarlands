bf74b8621b3f2b54da6138ec93df0e52d77da9ac
Prabakaran V <prabakaranpbk@gmail.com>
2023-09-09 12:17:03 +0530

    Preorder commented

--- APSGARLANDS/Themes/Storefront/views/public/checkout/create.blade.php
+++ APSGARLANDS/Themes/Storefront/views/public/checkout/create.blade.php
@@ -19,7 +19,7 @@
                                     <div class="checkout-form">
                                         @include('public.checkout.create.form.account_details')
                                         @include('public.checkout.create.form.billing_details')
-                                        @include('public.checkout.create.form.delivery_details')
+                                        {{-- @include('public.checkout.create.form.delivery_details') --}}
                                         @include('public.checkout.create.form.shipping_details')
                                         @include('public.checkout.create.form.order_note')
                                     </div>
